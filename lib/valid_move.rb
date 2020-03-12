12a006f4cc029a2c02a2bea45afe2663be1ba998
Jacinta Calixte <jacinta246@yahoo.com>
2020-03-12 00:13:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,18 @@
 def valid_move?(board, index)
+
 #if board [index] .between (0-8) && position_taken == true
 #check if position is not taken
-
-  ## we may want to CALL position_taken? from in here
-end
-def position_taken? (board, index)
+if index.between?(0,8) && position_taken?(board, index)
+ return true
+else
+  return false
+ end
+end 
+def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
     return true
  else
    return false
 end
  end
+valid_move?(1,2)
