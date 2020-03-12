1de73aca5feee8bf5d31362c5c65896e98d4d91c
Jacinta Calixte <jacinta246@yahoo.com>
2020-03-12 00:15:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,12 +2,12 @@ def valid_move?(board, index)
 
 #if board [index] .between (0-8) && position_taken == true
 #check if position is not taken
-if index.between?(0,8) && position_taken?(board, index)
+if index.between?(0,8) && !position_taken?(board, index)
  return true
 else
   return false
  end
-end 
+end
 def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
     return true
